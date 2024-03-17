6272ee9a8ab6122b6ee67b58cbf675b26a896b48
Kamil75902 <105596542+Kamil75902@users.noreply.github.com>
2024-03-17 19:53:30 +0100

    Fix add post modal

--- src/Components/AddPostModal.tsx
+++ src/Components/AddPostModal.tsx
@@ -36,3 +36,98 @@ const AddPostModal = (props: ModalProps) => {
         .min(40, "Minimum 40 characters")
         .max(1000, "Maximum 1000 characters"),
     }),
+ 
+    onSubmit: () => {
+      setIsLoading(true);
+      addDoc(postsCollectionRef, {
+        title: formik.values.title,
+        body: formik.values.description,
+        date: new Date().toDateString(),
+        comments: [],
+      })
+        .then(response => {
+          const storageRef = ref(storage, response.id);
+          uploadBytesResumable(storageRef, inputRef.current.files[0]);
+          props.onClose();
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    },
+  });
+  return (
+<Dialog onClose={props.onClose} open={props.open}>
+<Box
+        sx={{
+          backgroundColor: "white",
+          width: 500,
+          height: "70vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "15px",
+          padding: 10,
+          overflowY: "scroll",
+        }}
+>
+<Typography variant="h4">Add New Post</Typography>
+<TextField
+          value={formik.values.title}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          error={formik.touched.title && !!formik.errors.title}
+          helperText={formik.errors.title}
+          name="title"
+          fullWidth
+          label="Title"
+        />
+<TextField
+          value={formik.values.description}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          name="description"
+          error={formik.touched.description && !!formik.errors.description}
+          helperText={formik.errors.description}
+          multiline
+          fullWidth
+          label="Description"
+        />
+<label
+          style={{
+            backgroundColor: "#1976d2",
+            color: "#fff",
+            padding: "10px",
+            cursor: "pointer",
+            width: "100%",
+            textAlign: "center",
+          }}
+          htmlFor="file"
+>
+          Add file
+</label>
+<input
+          ref={inputRef}
+          type="file"
+          style={{ display: "none" }}
+          name="file"
+          id="file"
+        />
+ 
+        <Button
+          disabled={isLoading}
+          fullWidth
+          style={{ backgroundColor: "maroon", color: "white" }}
+          onClick={() => formik.handleSubmit()}
+>
+          {isLoading ? "Loading ..." : "Save"}
+</Button>
+<Button onClick={props.onClose} variant="contained" fullWidth>
+          Close
+</Button>
+</Box>
+</Dialog>
+  );
+};
+ 
+export default AddPostModal;
